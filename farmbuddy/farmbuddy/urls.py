4d16eaeff818033fbdb6825bc364604903039f25
Cynthiataragon <cynthiataragon@gmail.com>
2022-07-21 10:01:35 +0300

    Frixed bugs

--- farmbuddy/farmbuddy/urls.py
+++ farmbuddy/farmbuddy/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
+from users import views as user_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.home, name='home'),
