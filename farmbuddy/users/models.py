9e3b14cc2eb917e23b868ddf5bf06b9f904afb3a
Cynthiataragon <cynthiataragon@gmail.com>
2022-07-27 07:23:33 +0300

    changed the models.py file making the db readable

--- farmbuddy/users/models.py
+++ farmbuddy/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#climate and soil 
+
+class Dataset(models.Model):
+	location = models.CharField(max_length= 200)
+	soil = models.CharField(max_length= 200)
+	weather = models.CharField(max_length= 200)
+	crop = models.CharField(max_length= 200)
+
+	
+	#making the database readable
+	def __str__(self):
+		return self.location
